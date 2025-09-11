13ecd95ea936d064417baf87d1359df35300bcc2
Isaac Shin <ishin7921@gmail.com>
2025-09-11 15:05:21 -0500

    S3 config almost finished, waiting for CF config

--- terraform/modules/frontend/s3.tf
+++ terraform/modules/frontend/s3.tf
@@ -1,3 +1,58 @@
+# Create S3 bucket
 resource "aws_s3_bucket" "website_bucket" {
-  
+    bucket = "crctestbucket1"
+
+    tags = {
+      Name = "First test tag"
+    }
+}
+
+# Allow public access - Disabled for now to prepare for CloudFront
+# resource "aws_s3_bucket_acl" "acl" {
+#     bucket = aws_s3_bucket.website_bucket.id
+#     acl = "public-read"
+# }
+
+# Enable versioning
+resource "aws_s3_bucket_versioning" "versioning" {
+    bucket = aws_s3_bucket.website_bucket.id
+    versioning_configuration {
+      status = "Enabled"
+    }
+}
+
+# Website configuration
+resource "aws_s3_bucket_website_configuration" "website_config" {
+    bucket = aws_s3_bucket.website_bucket.id
+
+    index_document {
+      suffix = "index.html"
+    }
+}
+
+# Bucket Policy
+resource "aws_s3_bucket_policy" "bucket_policy"{
+    bucket = aws_s3_bucket.website_bucket.id
+
+    policy = jsonencode({
+        Version = "2012-10-17"
+        Statement = [
+            {
+                Effect = "Allow"
+                Principal = "*"
+                Action = "s3:GetObject"
+                Resource = "${aws_s3_bucket.website_bucket.arn}/*"
+            }
+        ]
+    })
+}
+
+
+# --- Objects ---
+# Index page
+resource "aws_s3_object" "index_html"{
+    bucket = aws_s3_bucket.website_bucket.id
+    key = "index.html"
+    source = "${path.root}/../website/index.html"
+    content_type = "text/html"
 }
