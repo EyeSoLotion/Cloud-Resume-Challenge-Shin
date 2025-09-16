1bed47fe5e88ba3f77ad3d9c1734ab571641395b
Isaac Shin <ishin7921@gmail.com>
2025-09-16 17:49:09 -0500

    Polished website, added dark mode toggle

--- website/script.js
+++ website/script.js
@@ -54,4 +54,25 @@ document.addEventListener('DOMContentLoaded', function(){
             }
         });
     });
+
+    // Theme toggle switch
+    let darkmode = localStorage.getItem('darkmode')
+    const themeSwitch = document.getElementById('theme-toggle')
+
+    const enableDarkMode = () => {
+        document.body.classList.add('darkmode')
+        localStorage.setItem('darkmode', 'active')
+    }
+
+    const disableDarkMode = () => {
+        document.body.classList.remove('darkmode')
+        localStorage.setItem('darkmode', null)
+    }
+
+    if(darkmode === "active") enableDarkMode()
+
+    themeSwitch.addEventListener("click", () => {
+        darkmode = localStorage.getItem('darkmode')
+        darkmode !== "active" ? enableDarkMode() : disableDarkMode()
+    })
 });
